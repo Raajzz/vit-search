9f4f11fa8e14b8f3e394c6c5a7b385cdf7303398
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-06-30 10:44:39 +0530

    Project completed, updated README, added .gitignore

--- README.md
+++ README.md
@@ -12,4 +12,10 @@ Linear search will work, but it'll be tedious to search every single string over
 Hashing is plausible but still for sizes in thousands upon thousands, we have to do better! And that's where tries come into picture and in this project, I'll be using Tries to solve to problem of searching VITians!
 <br>
 <br>
-The main objective of the project is to insert the details of VITians and retrieve them, the details however aren't going to persist, I do however wish to find a way to persist data in the future, either by using a file or in a database (locally) or in the cloud.
+The main objective of the project is to insert the details of VITians and search them, the details however aren't going to persist, I do however wish to find a way to persist data in the future, either by using a file or in a database (locally) or in the cloud.
+
+## IMPROVEMENTS TO BE MADE
+- Rewriting the whole project without `using namespace std;` as it destroyes the purpose of namespace.
+- The data fields of `VitianNode` are public, which is not desirable in the long run, they should be made private with necessary getters and setters (or putters).
+- Using regular expression to verify whether the string entered by the client is correct (VIT uses a 9 Letter system, where the first two characters are the year of joining, next three is the course undertaken and last 4 is the number, for instance, Ex: 20BCE0138)
+- Ability to batch insert multiple students by reading from a file or from a data stream would be desirable too. Using a cloud platform to store all the VITians data in the form of blobs with indexed web-links is an approach I'm willing to look into.
--- vit-search.cpp
+++ vit-search.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
-// since we're just doing a demo project, we're using std namespace globally to increase some time, this will be re-written later
+// since I'm just doing a demo project, I'm using std namespace throughout the project to provide some productivity, this will be re-written later without using namespace std.
 using namespace std;
 
 class Vitian{
@@ -84,45 +85,105 @@ public:
 };
 
 class VitianNode{
-private:
 public:
+  char reg_no_char;
   unordered_map<char, VitianNode*> children;
   Vitian* contents;
+
+  VitianNode(char reg_no_char){
+    this->reg_no_char = reg_no_char;
+    contents = nullptr;
+  }
 };
 
 class Trie{
+
 private:
-  VitianNode* node;
+  VitianNode* root;
+
 public:
-  Trie(char letter){
-    node->children[letter] = nullptr;
+  Trie(){
+    root = new VitianNode('\0');
   }
 
-  void insertion(string reg_no){
-    // VitianNode* copy_node = node;
-    // for(int i=0; i<reg_no.length()-1; i++){
-    //   if(copy_node->children[reg_no[i]]){
+  void insert(string reg_no){
+    VitianNode* copy_root = root;
+    for(int i=0; i<reg_no.size(); i++){
+      // to check whether a character exists
 
-    //   } else {
+      if(copy_root->children.count(reg_no[i])){
+        copy_root = copy_root->children[reg_no[i]];
+      } else {
+        VitianNode* copy_root_new = new VitianNode(reg_no[i]);
+        copy_root->children[reg_no[i]] = copy_root_new;
+        copy_root = copy_root_new;
+      }
 
-    //   }
-    // }
-    VitianNode* contents = nullptr;
-    // if(isEnd){
-    //   // inputs...
-    // } 
+    }
     
+    string name;
+    int current_year;
+    string course;
+    string school;
+    string specialization;
+
+    cout << "Enter Name :";
+    cin>>name;
+    cout << "Enter Current Year of Study :";
+    cin>>current_year;
+    cout << "Enter Course Name :";
+    cin>>course;
+    cout << "Enter School Name :";
+    cin>>school;
+    cout << "Enter Specialization :";
+    cin>>specialization;
+
+    copy_root->contents = new Vitian(name, reg_no, current_year, course, school, specialization);
+  }
+
+  Vitian* find(string reg_no){
+    VitianNode* copy_root = root;
+    for(int i=0; i<reg_no.size(); i++){
+      if(copy_root->children.count(reg_no[i])){
+        copy_root = copy_root->children[reg_no[i]];
+      } else {
+        return copy_root->contents;
+      }
+    }
+    return copy_root->contents;
   }
 
 };  
 
 int main(){
   
+  Trie vit_search;
+
+  int choice = 1;
+
+  while(choice){
+    cout << "\nEnter Your Choice:\n0 => End \n1 => Insertion \n2 => Searching\nChoice :";
+    cin>>choice;
+    string reg_no;
+    if(choice ==  1){
+      cout << "Enter Registration Number :";
+      cin>>reg_no;
+      vit_search.insert(reg_no);
+    } else if(choice == 2){
+      cout << "Enter Registration Number :";
+      cin>>reg_no;
+      Vitian* vitian_obj = vit_search.find(reg_no);
+      cout << "\nDetails are as follows,\n";
+      cout << "Name :" << vitian_obj->get_name() << "\n";
+      cout << "Registration Number :" << vitian_obj->get_reg_no() << "\n";
+      cout << "Current Year :" << vitian_obj->get_current_year() << "\n";
+      cout << "Course :" << vitian_obj->get_course() << "\n";
+      cout << "School :" << vitian_obj->get_school() << "\n";
+      cout << "Specialization: " << vitian_obj->get_specialization() << "\n";
+    }
+  }
+
+  cout << "\nThank you for stopping by <3 \nCome back soon!";
+
   return 0;
 }
-
-/*
-
-There's gonna be n nodes, now each node will have a u_map within it that'll store the children
-
-*/
